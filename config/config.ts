3f20edfd77627ce3e5a4ef80deec951e6d65c3f9
Taufik Januar <taufikjanuar35@gmail.com>
2020-06-20 17:33:49 +0700

    CHANGE MONGO DATABSE NAME AND HOST URL HERE

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,7 @@ const env = Deno.env.toObject();
 export const PORT = parseInt(env.PORT, 10) || 8080;
 
 // change "yourhosturl" with your host url
-export const MONGO_HOST_URL = env.MONGO_HOST_URL || "<yourhosturl>";
+export const MONGO_HOST_URL = env.MONGO_HOST_URL || "<your_HOST_URL>";
 
 // change "yourdb" with your database name
-export const MONGO_DB_NAME = env.MONGO_DB_NAME || "<yourdb>";
+export const MONGO_DB_NAME = env.MONGO_DB_NAME || "<your_DB>";
