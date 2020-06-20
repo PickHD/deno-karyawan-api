475f39fb40d19f1e434545999b65433146793501
PickHD <taufikjanuar35@gmail.com>
2020-06-20 17:07:58 +0700

    Create ROuter

---
+++ router.ts
@@ -0,0 +1,22 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+import {
+  createKaryawan,
+  getKaryawanWithId,
+  getAllKaryawan,
+  updateKaryawanWithId,
+  deleteKaryawanWithId,
+} from "./controllers/karyawanController.ts";
+
+import requiresBody from "./middleware/requiresBody.ts";
+
+const router = new Router();
+
+router
+  .get("/employees", getAllKaryawan)
+  .post("/employees", requiresBody, createKaryawan)
+  .get("/employees/:_id", getKaryawanWithId)
+  .put("/employees/:_id", requiresBody, updateKaryawanWithId)
+  .delete("/employees/:_id", deleteKaryawanWithId);
+
+export default router;
